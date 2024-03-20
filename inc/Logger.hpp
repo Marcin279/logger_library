026d33d93edd21abb06245430ecebc9f10380026
Marcin279 <marcin.biela12@gmail.com>
2024-03-20 23:45:00 +0100

    Configure project structure with googletest configuration

---
+++ inc/Logger.hpp
@@ -0,0 +1,8 @@
+//
+// Created by marci on 20.03.2024.
+//
+
+#ifndef LOGGER_APP_LOGGER_HPP
+#define LOGGER_APP_LOGGER_HPP
+
+#endif //LOGGER_APP_LOGGER_HPP
---
+++ src/Logger.cpp
@@ -0,0 +1,3 @@
+//
+// Created by marci on 20.03.2024.
+//
